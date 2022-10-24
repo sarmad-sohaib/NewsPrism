d1aa3f6c9f3345f4bca007121af183877ef788de
Sarmad Sohaib <62813277+sarmad-sohaib@users.noreply.github.com>
2022-10-24 17:09:52 +0500

    all fragments created

---
+++ app/src/main/java/com/sarmad/newsprism/savednews/SavedNewsFragment.kt
@@ -0,0 +1,19 @@
+package com.sarmad.newsprism.savednews
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sarmad.newsprism.R
+
+class SavedNewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_news, container, false)
+    }
+}
