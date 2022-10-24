04eaf76bc1802c3cb6b2082faff532c0598b28a0
Sarmad Sohaib <62813277+sarmad-sohaib@users.noreply.github.com>
2022-10-24 16:29:24 +0500

    Added viewBinding

--- app/src/main/java/com/sarmad/newsprism/MainActivity.kt
+++ app/src/main/java/com/sarmad/newsprism/MainActivity.kt
@@ -2,8 +2,10 @@ package com.sarmad.newsprism
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sarmad.newsprism.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
